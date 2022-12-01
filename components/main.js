745c183f62c0522d06af468bd5e514642e76ee84
olroque <88515335+olroque@users.noreply.github.com>
2022-12-01 17:56:40 -0500

    got creator description section up.

--- components/main.js
+++ components/main.js
@@ -13,8 +13,17 @@ export default function Main() {
                 </div>
                 <div className="job-title-container">
                     <p className="job-title">
-                        UX/UI Designer & <br/> Web Developer
+                        UX/UI Designer &amp; <br/> Web Developer
                     </p>
+                </div>
+            </section>
+            <section className="creator-description-container">
+                <div>
+                    <h4 className="creator-description">
+                        DOLD means ‘hidden’ is Swedish.
+                        Why hidden, you ask? Our aim is to uncover the simple, strong, undeniable truth of each brand.
+                        To find it, we have to ask the right questions, research, rethink, re-everything. This is the only way to ensure that our clients get that simple, powerful visual solution they've been looking for.
+                    </h4>
                 </div>
             </section>
         </div>
