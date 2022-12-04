130388d3469529713cb234c4b1560956916e1015
olroque <88515335+olroque@users.noreply.github.com>
2022-12-04 10:54:03 -0500

    gallery started - Link isn't playing nice.

--- components/gallery.js
+++ components/gallery.js
@@ -1,7 +1,46 @@
+import Link from "next/link"
 
 export default function Gallery() {
     return (
-        <section>
-        </section>
+        <div className="root">
+            <section className="gallery-container">
+                <div className="gallery-row">
+                    <div className="gallery-item portrait-pic">
+                        {/* <Link href="/"> */}
+                            <img src="/images/5.jpg" alt="picture of web developement project" />
+                        {/* </Link> */}
+                    </div>
+                    <div className="gallery-item landscape-pic">
+                        {/* <Link href="/"> */}
+                            <img src="/images/2.jpg" alt="picture of web developement project" />
+                        {/* </Link> */}
+                    </div>
+                </div>
+                <div className="gallery-row">
+                    <div className="gallery-item landscape-pic">
+                        {/* <Link href="/"> */}
+                            <img src="/images/3.jpg" alt="picture of web developement project" />
+                        {/* </Link> */}
+                    </div>
+                    <div className="gallery-item portrait-pic">
+                        {/* <Link href="/"> */}
+                            <img src="/images/8.jpg" alt="picture of web developement project" />
+                        {/* </Link> */}
+                    </div>
+                </div>
+                <div className="gallery-row">
+                    <div className="gallery-item portrait-pic">
+                        {/* <Link href="/"> */}
+                            <img src="/images/9.jpg" alt="picture of web developement project" />
+                        {/* </Link> */}
+                    </div>
+                    <div className="gallery-item landscape-pic">
+                        {/* <Link href="/"> */}
+                            <img src="/images/1.jpg" alt="picture of web developement project" />
+                        {/* </Link> */}
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/header'
 import Main from '../components/main'
+import Gallery from '../components/gallery'
 
 export default function Home() {
   return (
     <div >
       <Header />
       <Main />
+      <Gallery />
     </div>
   )
 }
