a43fe84051e760b69d55fd9553f6b813aa1d4a0b
olroque <88515335+olroque@users.noreply.github.com>
2022-12-08 13:53:09 -0500

    contact page solid for desktop.

---
+++ components/contactTop.js
@@ -0,0 +1,34 @@
+
+export default function() {
+    return (
+        <div className="root">
+            <div className="contact-page-container">
+                <div className="contact-page-name-container">
+                    <div>
+                        <h1>Contact Me</h1>
+                    </div>
+                </div>
+                <div className="contact-page-info-container">
+                    <div className="contact-phone">
+                        <p className="contact-info-name">Phone:</p>
+                        <p className="contact-info-item">252-665-3363</p>
+                    </div>
+                    <div className="contact-location">
+                        <p className="contact-info-name">Location:</p>
+                        <p className="contact-info-item">Chapel Hill</p>
+                        <p className="contact-info-item">North Carolina</p>
+                    </div>
+                    <div className="contact-email">
+                        <p className="contact-info-name">Email:</p>
+                        <p className="contact-info-item">olroque@yahoo.com</p>
+                    </div>
+                </div>
+                <div className="contact-image-container">
+                    <div>
+                        <img src="/images/2.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,12 @@
 import Header from '../components/header';
+import ContactTop from '../components/contactTop';
 import Footer from '../components/footer';
 
 export default function Contact() {
     return (
         <div>
             <Header />
+            <ContactTop />
             <Footer />
         </div>
     )
