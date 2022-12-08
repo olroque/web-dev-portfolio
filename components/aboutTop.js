ea4abb3f167c9e9268718928ffb10a11f5ce62e9
olroque <88515335+olroque@users.noreply.github.com>
2022-12-08 18:05:21 -0500

    started working on about page.

---
+++ components/aboutTop.js
@@ -0,0 +1,32 @@
+
+export default function() {
+    return (
+        <div className="root">
+            <div className="about-top-container">
+                <div className="about-name">
+                    <h1>
+                        ABOUT
+                    </h1>
+                </div>
+                <div className="about-info">
+                    <p>
+                        This is DOLD. That sweet spot between smart and foolish, fun and fine, monochromatic and multicolor, flawless design and casual beauty. 
+                        <br />
+                        <br />
+                        Founded by Aisha Blake, DOLD specializes in
+                        <br />
+                        a wide range of project scales. From the private to the public sectors, from corporates to SMBs, we're ready to take on any challenge.
+                    </p>
+                </div>
+            </div>
+            <div className="about-image-container">
+                <div>
+                    <img src="/images/2.jpg" alt="" />
+                </div>
+            </div>
+            <div className="about-services-container">
+
+            </div>
+        </div>
+    )
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,12 @@
 import Header from '../components/header';
+import AboutTop from '../components/aboutTop';
 import Footer from '../components/footer';
 
 export default function About() {
     return (
         <div>
             <Header />
+            <AboutTop />
             <Footer />
         </div>
     )
